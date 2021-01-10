f4a9a6e839db641039f616710466e33db4460a8b
Peter Bryant <peter.bryant@thekeysupport.com>
2021-01-10 11:54:55 +0000

    serverless func: tidal event data

---
+++ functions/tidal-events.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+
+const API_ENDPOINT =
+  'https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/0543/TidalEvents?duration=1'
+const { OCI_API_KEY } = process.env
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT, {
+    headers: {
+      Accept: 'application/json',
+      'Ocp-Apim-Subscription-Key': OCI_API_KEY,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => ({
+      statusCode: 200,
+      body: data,
+    }))
+    .catch((error) => ({ statusCode: 422, body: String(error) }))
+}
